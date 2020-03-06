035bdea1c3b9b18d7dbd51169b5e54f88509a218
hi_go <me@clutte.red>
2020-03-07 01:05:58 +0900

    wip.

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,63 @@
 extern crate piston_window;
 use piston_window::*;
+
+
+const WINDOW_TITLE: &str = "LifeGame.rs";
+const WINDOW_SIZE: Size = Size{width: 640.0, height: 480.0};
+
+#[derive(Clone)]
+struct Cell{
+    alive: bool,
+    alive_in_next_gen: bool,
+}
+
+struct Field{
+    width: usize,
+    height: usize,
+    x_num: usize,
+    y_num: usize,
+    cell: Vec<Cell>,
+    cell_size: Size,
+}
+
+impl Field{
+    fn init() -> Field{
+        let xy_num = (64,48);
+        let wh = (WINDOW_SIZE.width as usize, WINDOW_SIZE.height as usize);
+        Field{
+            width: wh.0,
+            height: wh.1,
+            x_num: xy_num.0,
+            y_num: xy_num.1,
+            cell: vec![
+                    Cell{
+                        alive: false,
+                        alive_in_next_gen: false,
+                    };
+                    xy_num.0 * xy_num.1
+                ],
+            cell_size: Size{
+                width: (wh.0 / xy_num.0) as f64,
+                height: (wh.1 / xy_num.1) as f64,
+            }
+        }
+    }
+}
+
 fn main() {
-    let mut window: PistonWindow = WindowSettings::new("LifeGame.rs", (640, 480))
+    let mut window: PistonWindow = WindowSettings::new(WINDOW_TITLE, WINDOW_SIZE)
         .exit_on_esc(true)
+        .vsync(true)
+        .resizable(false)
+        .samples(4)
         .build()
         .unwrap_or_else(|e| { panic!("Failed to build PistonWindow: {}", e) });
-    let mut x = 0.0;
-    let mut y = 0.0;
+
     while let Some(e) = window.next() {
         window.draw_2d(&e, |_c, g, _d| {
             clear([0.0, 0.0, 0.0, 1.0], g);
-            x += 1.0;
-            x %= 640.0;
-            y += 1.0;
-            y %= 480.0;
-            rectangle([0.5, 1.0, 0.5, 0.5],
-                [x, y, 10.0, 10.0],
-                _c.transform, g);
+            let draw_state = _c.draw_state.blend(draw_state::Blend::Add);
+            //ここにゲームの処理何か書く
         });
     }
 }
