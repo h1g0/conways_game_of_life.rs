673fbfc3309e3f3edaeb68268b09f26156d557c3
hi_go <me@clutte.red>
2020-03-16 02:34:36 +0900

    WIP.

--- src/lifegame.rs
+++ src/lifegame.rs
@@ -7,8 +7,18 @@ struct Cell{
     alive: bool,
     alive_in_next_gen: bool,
 }
+enum Neighbor{
+    UpperLeft,
+    UpperMiddle,
+    UpperRight,
+    Left,
+    Right,
+    LowerLeft,
+    LowerMiddle,
+    LowerRight,
+}
 
-struct Field{
+pub struct Field{
     width: usize,
     height: usize,
     x_num: usize,
@@ -57,9 +67,80 @@ impl Field{
     pub fn set_state(&mut self, id: usize, alive:bool){
         self.cell[id].alive = alive;
     }
+    pub fn get_state(&self, id:usize)->bool{
+        if id >= self.cell.len(){
+            panic!("The cell[{}] is not exit!",id);
+        }
+        return self.cell[id].alive;
+    }
 
+    fn get_neighbor_id(self,id:usize, neighbor:Neighbor)->Option<usize>{
+        if id >= self.cell.len(){
+            return None;
+        }
+        let left = if id % self.x_num == 0{false}else{true};
+        let upper = if id - self.x_num < 0 {false}else{true};
+        let right = if id % self.x_num == self.x_num - 1{false}else{true};
+        let lower = if id + self.x_num >= self.cell.len(){false}else{true};
+        match neighbor{
+            Neighbor::UpperLeft=>
+            if upper&&left{
+                Some(id - self.x_num - 1)
+            }else{
+                None
+            },
+            Neighbor::UpperMiddle=>
+            if upper{
+                Some(id - self.x_num)
+            }else{
+                None
+            },
+            Neighbor::UpperRight=>
+            if upper&&right{
+                Some(id - self.x_num + 1)
+            }else{
+                None
+            },
+            Neighbor::Left=>
+            if left{
+                Some(id - 1)
+            }else{
+                None
+            },
+            Neighbor::Right=>
+            if right{
+                Some(id + 1)
+            }else{
+                None
+            },
+            Neighbor::LowerLeft=>
+            if lower&&left{
+                Some(id + self.x_num - 1)
+            }else{
+                None
+            },
+            Neighbor::LowerMiddle=>
+            if lower{
+                Some(id + self.x_num)
+            }else{
+                None
+            },
+            Neighbor::LowerRight=>
+            if lower&&right{
+                Some(id + self.x_num + 1)
+            }else{
+                None
+            },
+        }
+    }
+    fn get_neighbor_state(&self,id:usize,neighbor: Neighbor)->bool{
+        if let Some(neighbor_id) = self.get_neighbor_id(id, neighbor) {
+            return self.get_state(neighbor_id);
+        }else{
+            return false;
+        }
+    }
     pub fn set_state_for_all_cells(&mut self, alive:Vec<bool>){
-        // TODO: ここから
         for i in 0..self.cell.len(){
             if i < alive.len(){
                 self.cell[i].alive = alive[i];
@@ -85,98 +166,98 @@ impl Field{
 
 #[cfg(test)]
 mod tests {
-    mod cell{
-        #[test]
-        fn cell_dies_for_no_neighbor() {
-            let mut result;
-            let live = vec![
-                false,false,false,
-                false,true,false,
-                false,false,false,
-            ];
-            result = false;
-            assert_eq!(result,false);
+    #[test]
+    fn cell_dies_for_no_neighbor() {
+        let mut field = super::Field::new((3,3), (8,8));
+        let mut result;
+        let live = vec![
+            false,false,false,
+            false,true,false,
+            false,false,false,
+        ];
+        field.set_state_for_all_cells(live);
+        result = field.get_next_gen_state_for_one_cell(4);
+        assert_eq!(result,false);
 
-            let dead = vec![
-                false,false,false,
-                false,false,false,
-                false,false,false,
-            ];
-            result = false;
-            assert_eq!(result,false);
-        }
-        #[test]
-        fn cell_dies_for_1_neighbor() {
-            let mut result;
-            let live = vec![
-                false,true,false,
-                false,true,false,
-                false,false,false,
-            ];
-            result = false;
-            assert_eq!(result,false);
+        let dead = vec![
+            false,false,false,
+            false,false,false,
+            false,false,false,
+        ];
+        result = false;
+        assert_eq!(result,false);
+    }
+    #[test]
+    fn cell_dies_for_1_neighbor() {
+        let mut result;
+        let live = vec![
+            false,true,false,
+            false,true,false,
+            false,false,false,
+        ];
+        result = false;
+        assert_eq!(result,false);
 
-            let dead = vec![
-                false,true,false,
-                false,false,false,
-                false,false,false,
-            ];
-            result = false;
-            assert_eq!(result,false);
-        }
-        #[test]
-        fn cell_lives_for_2_neighbors() {
-            let mut result;
-            let live = vec![
-                false,true,true,
-                false,true,false,
-                false,false,false,
-            ];
-            result = true;
-            assert_eq!(result,true);
-            let dead = vec![
-                false,true,true,
-                false,false,false,
-                false,false,false,
-            ];
-            result = false;
-            assert_eq!(result,false);
-        }
-        #[test]
-        fn cell_lives_borns_for_3_neighbors() {
-            let mut result;
-            let live = vec![
-                false,true,true,
-                false,true,false,
-                false,true,false,
-            ];
-            result = true;
-            assert_eq!(result,true);
-            let dead = vec![
-                false,true,true,
-                false,false,false,
-                false,true,false,
-            ];
-            result = true;
-            assert_eq!(result,true);
-        }
-        #[test]
-        fn cell_dies_for_4_or_more_neighbors(){
-            let mut result;
-            let live = vec![
-                false,true,true,
-                false,true,false,
-                false,true,true,
-            ];
-            result = false;
-            assert_eq!(result,false);
-            let dead = vec![
-                false,true,true,
-                false,false,false,
-                false,true,true,
-            ];
-            result = false;
-            assert_eq!(result,false);
-        }
+        let dead = vec![
+            false,true,false,
+            false,false,false,
+            false,false,false,
+        ];
+        result = false;
+        assert_eq!(result,false);
+    }
+    #[test]
+    fn cell_lives_for_2_neighbors() {
+        let mut result;
+        let live = vec![
+            false,true,true,
+            false,true,false,
+            false,false,false,
+        ];
+        result = true;
+        assert_eq!(result,true);
+        let dead = vec![
+            false,true,true,
+            false,false,false,
+            false,false,false,
+        ];
+        result = false;
+        assert_eq!(result,false);
+    }
+    #[test]
+    fn cell_lives_borns_for_3_neighbors() {
+        let mut result;
+        let live = vec![
+            false,true,true,
+            false,true,false,
+            false,true,false,
+        ];
+        result = true;
+        assert_eq!(result,true);
+        let dead = vec![
+            false,true,true,
+            false,false,false,
+            false,true,false,
+        ];
+        result = true;
+        assert_eq!(result,true);
+    }
+    #[test]
+    fn cell_dies_for_4_or_more_neighbors(){
+        let mut result;
+        let live = vec![
+            false,true,true,
+            false,true,false,
+            false,true,true,
+        ];
+        result = false;
+        assert_eq!(result,false);
+        let dead = vec![
+            false,true,true,
+            false,false,false,
+            false,true,true,
+        ];
+        result = false;
+        assert_eq!(result,false);
     }
 }
